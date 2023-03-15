1490c8485ea4fe1c7f14f5bb7dfe204880abd905
IagoVinicius12 <115949326+IagoVinicius12@users.noreply.github.com>
2023-03-15 15:41:08 -0300

    Add files via upload

---
+++ mainp.cpp
@@ -0,0 +1,15 @@
+#include"iostream"
+#include"fstream"
+#include"funcs.hpp"
+using namespace std;
+int main(){
+    ifstream arq1;
+    Funcs f;
+    arq1.open("input.data"); 
+    int i=0;
+    // while(i<4){
+         f.ler();
+    //     i++;
+    // }
+    //fechar();
+}
