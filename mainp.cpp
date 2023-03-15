39267544e341ef78e56e9740f0facf1821a15f29
IagoVinicius12 <115949326+IagoVinicius12@users.noreply.github.com>
2023-03-15 15:43:55 -0300

    Update mainp.cpp

--- mainp.cpp
+++ mainp.cpp
@@ -3,13 +3,6 @@
 #include"funcs.hpp"
 using namespace std;
 int main(){
-    ifstream arq1;
     Funcs f;
-    arq1.open("input.data"); 
-    int i=0;
-    // while(i<4){
-         f.ler();
-    //     i++;
-    // }
-    //fechar();
+       f.ler();
 }
